2c367f4d2a6daf5fa5fbad541cbb0aa97ba22285
Tim Acosta <timacosta06@gmail.com>
2021-09-16 17:59:31 +0100

    initial dependency setup

--- app/src/main/java/com/acostim/brastlewark/ui/MainActivity.kt
+++ app/src/main/java/com/acostim/brastlewark/ui/MainActivity.kt
@@ -1,8 +1,12 @@
-package com.acostim.brastlewark
+package com.acostim.brastlewark.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.acostim.brastlewark.R
+import dagger.hilt.android.HiltAndroidApp
 
+
+@HiltAndroidApp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
