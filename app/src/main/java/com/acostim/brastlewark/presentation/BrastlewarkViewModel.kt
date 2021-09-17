a5beb10aade5fced3c219bf76d6b2a9c1163bfe3
Tim Acosta <timacosta06@gmail.com>
2021-09-18 00:47:10 +0100

    viewmodel

---
+++ app/src/main/java/com/acostim/brastlewark/presentation/BrastlewarkViewModel.kt
@@ -0,0 +1,39 @@
+package com.acostim.brastlewark.presentation
+
+
+import androidx.lifecycle.*
+import com.acostim.brastlewark.core.Resource
+import com.acostim.brastlewark.domain.BrastlewarkRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
+
+@HiltViewModel
+class BrastlewarkViewModel
+@Inject
+constructor(
+    private val repository: BrastlewarkRepository,
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val currentGnomeName = savedStateHandle.getLiveData<String>("gnomeName", "Tobus Quickwhistle")
+
+    fun setGnome(gnomeName: String) {
+        currentGnomeName.value = gnomeName
+    }
+
+    val fetchGnomeList = currentGnomeName.distinctUntilChanged().switchMap { gnomeName ->
+        liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
+            emit(Resource.Loading)
+            try {
+                repository.getAllGnomes().collect {
+                    emit(it)
+                }
+            } catch (e: Exception) {
+                emit(Resource.Failure(e))
+            }
+        }
+    }
+
+}
