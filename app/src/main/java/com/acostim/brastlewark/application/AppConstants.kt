cd16bb61a7b1adc38094c9461beb8aea9cd20c5a
Tim Acosta <timacosta06@gmail.com>
2021-09-17 18:52:39 +0100

    created file to store all constants

---
+++ app/src/main/java/com/acostim/brastlewark/application/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.acostim.brastlewark.application
+
+object AppConstants {
+
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+
+
+    const val DATABASE_NAME = "brastlewark_table"
+
+}
