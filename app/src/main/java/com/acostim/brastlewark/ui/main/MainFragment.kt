653274ce0cb0d58b49fe139f435bfeaba95caa48
Tim Acosta <timacosta06@gmail.com>
2021-09-19 16:16:02 +0100

    mainfragment replaced due to changes in viewmodel

--- app/src/main/java/com/acostim/brastlewark/ui/main/MainFragment.kt
+++ app/src/main/java/com/acostim/brastlewark/ui/main/MainFragment.kt
@@ -1,11 +1,9 @@
 package com.acostim.brastlewark.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.acostim.brastlewark.R
@@ -13,6 +11,7 @@ import com.acostim.brastlewark.core.Resource
 import com.acostim.brastlewark.data.model.Gnome
 import com.acostim.brastlewark.databinding.MainFragmentBinding
 import com.acostim.brastlewark.presentation.BrastlewarkViewModel
+import com.acostim.brastlewark.presentation.ResourceState
 import com.acostim.brastlewark.ui.main.adapter.BrastlewarkAdapter
 import com.acostim.brastlewark.utils.extensions.ui.*
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,6 +25,8 @@ class MainFragment : Fragment(R.layout.main_fragment),
     private lateinit var adapter: BrastlewarkAdapter
     private val viewModel by activityViewModels<BrastlewarkViewModel>()
 
+    private var gnomeList = emptyList<Gnome>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
@@ -38,39 +39,62 @@ class MainFragment : Fragment(R.layout.main_fragment),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding = MainFragmentBinding.bind(view)
+        configureLayout(view)
+        configureSearchView()
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerView.adapter = adapter
+        setObservers()
 
-        binding.searchView.onQueryTextChanged {
-            viewModel.setGnome(it)
+        if (gnomeList.isEmpty()) {
+            viewModel.setResourceState(ResourceState.GetBrastlewarkPopulation)
         }
 
-        viewModel.fetchGnomeList.observe(viewLifecycleOwner, Observer { result ->
+    }
+
+    private fun configureLayout(view: View) {
+        binding = MainFragmentBinding.bind(view)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun configureSearchView() {
+        binding.searchView.onQueryTextChanged {
+            viewModel.setResourceState(ResourceState.GetFilteredBlastewarkPopulation(it, gnomeList))
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.resourceState.observe(viewLifecycleOwner, { result ->
+
             binding.progressBar.showIf { result is Resource.Loading }
 
-            when(result) {
+            when (result) {
+
                 is Resource.Loading -> {
                     binding.emptyContainer.root.hide()
                 }
+
                 is Resource.Success -> {
+                    gnomeList = result.data
+                    adapter.setGnomeList(gnomeList)
+
+                }
+                is Resource.FilteredGnomes -> {
                     if(result.data.isEmpty()) {
+                        adapter.setGnomeList(result.data)
                         binding.recyclerView.hide()
                         binding.emptyContainer.root.show()
-                        return@Observer
+                    } else {
+                        adapter.setGnomeList(result.data)
+                        binding.recyclerView.show()
+                        binding.emptyContainer.root.hide()
                     }
-                    binding.recyclerView.show()
-                    adapter.setGnomeList(result.data)
-                    binding.emptyContainer.root.hide()
                 }
+
                 is Resource.Failure -> {
                     showToast("Error during network call ${result.exception}")
                 }
             }
-
         })
-
     }
 
     override fun onGnomeClick(gnome: Gnome, position: Int) {
