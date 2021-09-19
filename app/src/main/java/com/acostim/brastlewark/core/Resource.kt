00314569f27dc39171a28bd07d96c72e77ed50df
Tim Acosta <timacosta06@gmail.com>
2021-09-19 16:12:10 +0100

    added FilteredGnomes resource state to handle filtered list

--- app/src/main/java/com/acostim/brastlewark/core/Resource.kt
+++ app/src/main/java/com/acostim/brastlewark/core/Resource.kt
@@ -3,5 +3,6 @@ package com.acostim.brastlewark.core
 sealed class Resource<out T> {
     object Loading: Resource<Nothing>()
     data class Success<out T>(val data: T) : Resource<T>()
+    data class FilteredGnomes<out T>(val data: T): Resource<T>()
     data class Failure(val exception: Exception) : Resource<Nothing>()
 }
