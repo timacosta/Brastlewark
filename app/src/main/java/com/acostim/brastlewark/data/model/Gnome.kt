062e7f679421857b2d4dd38cb5447de2470a7d70
Tim Acosta <timacosta06@gmail.com>
2021-09-20 02:57:46 +0100

    changed Parcelize package due to deprecation

--- app/src/main/java/com/acostim/brastlewark/data/model/Gnome.kt
+++ app/src/main/java/com/acostim/brastlewark/data/model/Gnome.kt
@@ -3,7 +3,7 @@ package com.acostim.brastlewark.data.model
 import android.os.Parcelable
 import com.acostim.brastlewark.data.local.GnomeEntity
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Gnome(
